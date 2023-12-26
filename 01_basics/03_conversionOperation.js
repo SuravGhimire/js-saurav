4e75ab0fccc30dbcc22dcbd00f9a3d72cff05dd6
SuravGhimire <154653168+SuravGhimire@users.noreply.github.com>
2023-12-26 15:59:19 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
 let score = "33"
 
-console.log(typeof score);
+// console.log(typeof score);
 // console.log(typeof(score));
 
 let valueInNumber = Number(score)
 
 // console.log(typeof valueInNumber );
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 // let IsBoolean = Boolean(score)
 
@@ -24,5 +24,43 @@ console.log(valueInNumber);
 let someNumber=33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *****************************Operations****************************************
+
+let value =3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(20%2);
+
+let str1 ="hello"
+let str2 =" saurav"
+
+let str3 = str1+str2
+// console.log(str3); //hello saurav
+
+// console.log("1"+"2");  //12
+// console.log("1"+ 2);//12
+// console.log(1+"2"+2); //122
+// console.log(1+2+"2"); //32
+
+
+// console.log(+true); //1
+// console.log(+"");   //0
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+
+// console.log(num1);  //4
+
+let gameCounter= 100
+ gameCounter ++ //101
+++gameCounter
+// console.log(gameCounter); // 101
