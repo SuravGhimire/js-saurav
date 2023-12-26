880118af33b701e5a8df5095076f017533ece30b
SuravGhimire <154653168+SuravGhimire@users.noreply.github.com>
2023-12-26 15:35:41 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber );
+
+console.log(valueInNumber);
+
+// let IsBoolean = Boolean(score)
+
+// console.log(IsBoolean);
+
+// "33" => 33 
+// "33abc" => NaN (not a number)
+// true=> 1; false => 0
+
+//1 => true ; 0=> false
+// "" => false
+// "saurav" => true
+
+let someNumber=33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
