f076d55ee663deeaa0fc5484aa366deb971dfc36
SuravGhimire <154653168+SuravGhimire@users.noreply.github.com>
2023-12-27 08:21:30 +0000

    comparision of datat

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2>1); //true
+// console.log(2>=1);  // true
+// console.log(2==1);   //false
+// console.log(2!=1);   //true
+
+
+//  console.log("2">1); //true
+//  console.log("02">1);  //true
+
+//  console.log(null > 0);  //false
+//  console.log(null >= 0);  //true
+//  console.log(null == 0);  //false
+
+ /*The reason is that an equality check == and comparisons > < >= <= wroks differently.
+   Comparisons convert null to number, treating it as 0.
+   That's why (2)null >= 0 is true and (1) null>0 is false.*/
+
+   console.log(undefined  == 0);  //false
+   console.log(undefined > 0);  //false
+   console.log(undefined < 0);  //false
+
+   // === not only value is checked but the data type is also checked (stricked checked)
+
+//    console.log("2"===2); //false
+
+
+
+
