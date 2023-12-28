414bdff6474c391083f1bb187d29d75460846108
SuravGhimire <154653168+SuravGhimire@users.noreply.github.com>
2023-12-28 04:18:10 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name ="saurav"
+const repoCount = 50
+
+// console.log(name + repoCount+ "Value"); //saurav50Value
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  //modern srting syntax(string intepollation )
+
+
