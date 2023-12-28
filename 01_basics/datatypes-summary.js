adac446cbc61497babdb5f9584de4e17f6695906
SuravGhimire <154653168+SuravGhimire@users.noreply.github.com>
2023-12-28 03:28:41 +0000

    summarys on datatypes

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,45 +1,70 @@
-// Primitive (datatype)
+ // Primitive (datatype)
 
-// 7 types : String , Number, Boolean ,Null, undefined, Symbol, BigInt
+ // 7 types : String , Number, Boolean ,Null, undefined, Symbol, BigInt
 
-const score = 100 
-const scoreValue = 100.3
+// const score = 100 
+// const scoreValue = 100.3
 
-const isLoggedIn =false
-const outsideTemp = null
-let userEmail ;
+// const isLoggedIn =false
+// const outsideTemp = null
+// let userEmail ;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
-console.log(id);
-console.log(typeof id);
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id);
+// console.log(typeof id);
 
- console.log(id==anotherId); //false
+//  console.log(id==anotherId); //false
 
-const bigNumber = 35995694654646n
-console.log(typeof bigNumber);
+// const bigNumber = 35995694654646n
+// console.log(typeof bigNumber);
 
 
 
-// Reference (Non Primitive)
+// // Reference (Non Primitive)
 
 // Array, Objects , Functions
 
-const heros = ["ironman","batman","thor"]
-console.log(heros);
-console.log(typeof heros);
+// const heros = ["ironman","batman","thor"]
+// console.log(heros);
+// console.log(typeof heros);
 
- let myObj=   {
-    name : "saurav",
-    age : 21,
+//  let myObj=   {
+//     name : "saurav",
+//     age : 21,
+// }
+
+
+// console.log(myObj.name);  //saurav
+// console.log(typeof myObj);  //object
+
+
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
+// console.log(typeof myFunction);
+
+
+//**********************Memorys in JS****************************
+
+// Stack(Primitive) , Heap (Non Primitive)
+
+let myName ="saurav.com"
+
+let anotherName = myName  //how a value can be changed
+anotherName ="chaiaurcode"
+
+console.log(myName);        // saurav.com
+console.log(anotherName);   //chaiaurcode
+
+let userOne ={
+    email: "user@google.com",
+    upi : "user@bl"
 }
 
+let userTwo =userOne
 
-console.log(myObj);
-console.log(typeof myObj);
+userTwo.email="saurav@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
 
-
-const myFunction = function(){
-    console.log("Hello World");
-}
-console.log(typeof myFunction);
