81aadae34e978d36b1cfa4c8f2d03519bc584168
saurav ghimire <154653168+SuravGhimire@users.noreply.github.com>
2024-10-21 08:30:13 +0000

    objects part 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -33,5 +33,19 @@ const regularUser ={
  }
 }
 
-console.log(regularUser.fullname)              //{ UserfullName: { firstName: 'saurav', lastName: 'ghimire' }
-console.log(regularUser.fullname.UserfullName)  //{ firstName: 'saurav', lastName: 'ghimire' }
+//console.log(regularUser.fullname)              //{ UserfullName: { firstName: 'saurav', lastName: 'ghimire' }
+//console.log(regularUser.fullname.UserfullName)  //{ firstName: 'saurav', lastName: 'ghimire' }
+
+//********************************************************************************************************************************************************************* */
+//COMBINING OBJECTS
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+
+//const obj3= {obj1,obj2}  //It gives the same problem as array ,as shown in below output
+//console.log(obj3);        //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+
+const obj3 = Object.assign({},obj1,obj2)   //empty value {} is taken in order to get all the possible values  , {} acts as target and other objects as a source.
+   //using Object.assig we can assign as many  as objects.
+//console.log(obj3);    // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+//************************************************************************************************************************************ */
