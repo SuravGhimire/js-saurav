27cfbae341070c785dbd0574a4414eb49ec214dd
saurav ghimire <154653168+SuravGhimire@users.noreply.github.com>
2024-12-07 03:49:04 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,36 @@
+//IIFE (Immesiately Invoked Function Expression)
+/*    Means immediate execution of function , when function
+      is written   */
+     
+ /*    function chai(){
+        console.log(`DB CONNECTED`);
+      }
+      chai() //DC CONNECTED */
+    
+ /*   (function chai(){
+        console.log(`DB CONNECTED`);
+      }() //ERROR   */
+    
+      //BUT USE OF IIFE :()()
+     (function chai(){
+        //named iife
+        console.log(`DB CONNECTED`);
+      })();  //DB CONNECTED
+
+// IIFE as an arrow function
+   /* (() => {
+        console.log(`DB CONNECTED TWO`);
+    })()   //Error ; since IIFE dont know when to stop ,while dealing with many fns So use a semi-colon ;
+   */
+
+    (() => {
+        console.log(`DB CONNECTED TWO`);
+    })(); //DB CONNECTED TWO
+   
+    //passing parameters
+    ((name) => {
+        console.log(`DB CONNECTED TWO ${name}`);
+    })('saurav'); //DB CONNECTED TWO saurav
+
+
+    
