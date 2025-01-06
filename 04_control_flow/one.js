b437f2ba9b310c21091b8fb11667a4ddfe587344
saurav ghimire <154653168+SuravGhimire@users.noreply.github.com>
2025-01-06 10:27:57 +0000

    if statement

---
+++ 04_control_flow/one.js
@@ -0,0 +1,32 @@
+//CONTOL FLOW::
+// if statement;;
+ 
+/* if(true){
+    
+} */
+// if(true){
+//    console.log("executed")    //exectued
+// }
+
+if(2=="2"){
+   // console.log("executed");    //executed
+}
+//BUT::
+
+if (2==="2"){
+   // console.log("executed");    //it doesnot execute the pgm
+}
+
+// here == only check condition ;; but === checks condn and type check:
+
+//comparision operators :: <,>,<=,>=,==,!=,===,!==(-ve sign check)
+
+const temperature= 40
+if (temperature < 50)
+{
+    console.log("less then 50");
+}
+console.log("temp is greater than 50.");
+
+/*  less then 50
+    temp is greater than 50.  */
