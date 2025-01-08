9e8f17afd2de02c304dff265772cbeb101105b9b
saurav ghimire <154653168+SuravGhimire@users.noreply.github.com>
2025-01-08 06:03:29 +0000

    for-of on maps

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -13,6 +13,29 @@ for (const num of arr) {
 
 const greetings = "Hello world !"
 for (const greet of greetings) {
-    console.log(`Each char is ${greet}`);
+    // console.log(`Each char is ${greet}`);
 }
 
+// Maps ::
+//  map itself is an object
+  /*The Map object holds key-value pairs and remembers the original insertion order of the keys.
+    Any value (both objects and primitive values) may be used as either a key or a value */
+
+const map = new Map()
+map.set('nep',"nepal")
+map.set('IN',"India")
+map.set('FR',"france")
+
+// console.log(map);
+//Map(3) { 'nep' => 'nepal', 'IN' => 'India', 'FR' => 'france' }
+
+for (const key of map) {
+    // console.log(key);
+}
+
+for (const [key,value] of map) {
+    console.log(key,':-', value);
+}
+/* nep :- nepal
+   IN :- India
+   FR :- france  */
