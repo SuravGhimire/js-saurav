feb7b1bee7f00a8ae34bd6b891c22c2ffcfe0398
saurav ghimire <154653168+SuravGhimire@users.noreply.github.com>
2025-01-08 11:14:30 +0000

    forEach loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,17 @@
+// continue.....
+
+//interesting loop ,which we use most of time for array.
+//FOR EACH loop::
+
+const coding = ["js","ruby","java","python","cpp"]
+
+// coding.forEach( function (val) {
+
+// console.log(val);
+
+// })  
+  /*js
+    ruby
+    java
+    python
+    cpp */ 
