14c664c0e7849c0d72287a73e727c003bedc68e9
saurav ghimire <154653168+SuravGhimire@users.noreply.github.com>
2025-01-08 11:24:35 +0000

    return value of forEach

---
+++ 05_iterations/six.js
@@ -0,0 +1,10 @@
+
+// const coding = ["js","ruby","java","python","cpp"]
+
+// const values = coding.forEach( (item) => {   // what if we want to store in a variable , to know what does foreach return vakue or if it return or not 
+//     console.log(item);
+//     return item  // -> and again it comes undefined.
+// })
+// console.log(values);
+// here undefined is the return value , so there is no value if we want to return value we have to return it manually ,as above. 
+// hence foreach is a undefined.
