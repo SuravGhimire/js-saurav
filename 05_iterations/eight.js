5fb5b3d96596ccb4cd355128864018168cc07b80
saurav ghimire <154653168+SuravGhimire@users.noreply.github.com>
2025-01-09 05:41:44 +0000

    reduce using arrow fn with example of :: shopingCart

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -6,7 +6,7 @@
   on each element of the array,in order ,passing in the return value from
   the calculation on the preceding element. */
 
-  const myNums=[1,2,3] 
+//   const myNums=[1,2,3] 
 
 //   const myTotal = myNums.reduce(function (acc,currentval){
 //     console.log(`acc: ${acc} and currentval: ${currentval}`);
@@ -19,3 +19,36 @@
     acc: 1 and currentval: 2
     acc: 3 and currentval: 3
     6 */
+
+//----------------------------------------------------------------------------------------------------
+
+// reduce using arrow function
+
+const myTotal = myNums.reduce( (acc,curr) => acc+curr, 5)
+
+// console.log(myTotal); //11
+
+ //example::
+
+ const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "python course",
+        price: 1999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data scientist course",
+        price: 12999
+    }
+ ]
+
+ const priceToPay = shoppingCart.reduce((acc,item) => acc+ item.price ,0)
+ console.log(priceToPay);  //23996
+ 
